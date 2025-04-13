3379098fdadff9b3f4fbd3825c56c93475539250
mightychild <69825325+mightychild@users.noreply.github.com>
2025-04-13 02:25:11 +0000

    Update app.js

--- nodeforge/backend/app.js
+++ nodeforge/backend/app.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB Atlas'))
-.catch((err) => console.error('There is error connecting to MongoDB Atlas:', err));
+.catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
 
 // Routes
 app.use('/api/auth', authRoutes);
