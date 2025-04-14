7fbbfdb332f13a47355537f53d3142cf2cafe633
mightychild <69825325+mightychild@users.noreply.github.com>
2025-04-14 10:26:28 +0100

    Update app.js

--- nodeforge/backend/app.js
+++ nodeforge/backend/app.js
@@ -20,5 +20,5 @@ app.use('/api/auth', authRoutes);
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`The Server is running on port ${PORT}`);
 });
