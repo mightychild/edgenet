921752e00307fea893b9fb78c1c3a0c431cbd5e9
mightychild <69825325+mightychild@users.noreply.github.com>
2025-04-17 09:57:22 +0100

    Update app.js

--- nodeforge/backend/app.js
+++ nodeforge/backend/app.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.use('/api/auth', authRoutes);
 
-// Start the server
+// Start t server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
