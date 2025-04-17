f54ecb45b0b25b581bd58bbbc1ee545544717390
mightychild <69825325+mightychild@users.noreply.github.com>
2025-04-17 09:56:09 +0100

    Update app.js

--- nodeforge/backend/app.js
+++ nodeforge/backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Connect to MongoDB Atlas
+// Connect to MongoDB 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
